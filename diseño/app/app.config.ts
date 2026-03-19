b1c0aac1f848ff6b263db6a0765d1328d96f38c6
jairemiliohermar <jairemilioher@micorreo.upp.edu.mx>
2026-03-18 21:30:21 -0600

    Create app.config.ts

---
+++ diseño/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http'; // 👈 Importación necesaria
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient() // 👈 Habilita la comunicación con tu servidor Node.js
+  ]
+};
